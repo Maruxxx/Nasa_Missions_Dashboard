fbf40827e941fde9a3afb904f6c576ca5d97a287
Maruxxx <marux.work@gmail.com>
2023-01-15 04:16:43 +0100

    ..

--- server/api/server.js
+++ server/api/server.js
@@ -15,7 +15,7 @@ async function serverStart() {
     await loadPlanetsData();
     await loadLaunchesData();
     server.listen(port, () => console.log(`Listening to port ${port}`))
-    //hey
+    //heyy
 }
 
 serverStart()
